36511eee7da3a5459d552a0169e9c0f8c261e388
lin <lin302010@qq.com>
2021-04-19 08:05:59 +0800

    修改打包后html资源引入路径

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,13 @@
  * @Date: 2021-04-15 16:33:33
  * @Author: zb.lin
  * @Email: lin302010@qq.com
- * @LastEditTime: 2021-04-19 07:37:24
+ * @LastEditTime: 2021-04-19 08:03:52
  */
 const webpack = require('webpack');
 module.exports = {
 
-    publicPath: process.env.VUE_APP_TEMPLATE_URL,
+    // publicPath: process.env.VUE_APP_TEMPLATE_URL,
+    publicPath: '/dist/',
     //支持jquery
     chainWebpack: config => {
         config
