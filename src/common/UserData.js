f388bb3f6cfc9750e4f25af38894adb6b0a77470
zb.lin <lin302010@qq.com>
2025-12-06 07:42:50 +0800

    Add new user data record for December

--- src/common/UserData.js
+++ src/common/UserData.js
@@ -14,6 +14,7 @@ export const usersData = {
             { record_date: "2025-11-08", height: 114 },
             { record_date: "2025-11-17", height: '', weight: 20.7 },
             { record_date: "2025-11-22", height: 114.8 },
+            { record_date: "2025-12-06", height: 115.5 },
         ]
     },
     yaya: {
