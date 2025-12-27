7950a4ca680520b45b122b22be0ab6a504c03d23
zb.lin <lin302010@qq.com>
2025-12-27 15:08:47 +0800

    Update UserData.js

--- src/common/UserData.js
+++ src/common/UserData.js
@@ -16,7 +16,6 @@ export const usersData = {
             { record_date: "2025-11-22", height: 114.8 },
             { record_date: "2025-12-06", height: 115.5 },
             { record_date: "2025-12-14", weight: 19.7 },
-            { record_date: "2025-12-21", height: 113 },
             { record_date: "2025-12-27", height: 114.8 },
 
         ]
@@ -34,7 +33,6 @@ export const usersData = {
             { record_date: "2025-11-08", height: 91, weight: 15 },
             { record_date: "2025-11-22", height: 92, weight: '' },
             { record_date: "2025-12-14", weight: 14.4 },
-            { record_date: "2025-12-21", height: 90.2 },
             { record_date: "2025-12-27", height: 91.8 },
 
         ]
