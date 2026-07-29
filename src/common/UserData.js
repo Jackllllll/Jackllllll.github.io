c6953ea684736c453a3634125dbecf8e96e0d731
zb.lin <lin302010@qq.com>
2026-07-29 09:34:56 +0800

    Update UserData.js

--- src/common/UserData.js
+++ src/common/UserData.js
@@ -28,6 +28,7 @@ export const usersData = {
             { record_date: "2026-06-29", height: 117.0 },
             { record_date: "2026-07-04", height: 117.3 },
             { record_date: "2026-07-21", height: 117.5 },
+            { record_date: "2026-07-29", height: 117.6 },
         ]
     },
     yaya: {
@@ -52,6 +53,7 @@ export const usersData = {
             { record_date: "2026-05-06", weight: 13.5 },
             { record_date: "2026-06-01", height: 94.2 },
             { record_date: "2026-06-29", height: 94.9 },
+            { record_date: "2026-06-29", height: 95.6 },
         ]
     },
     linlin: {
